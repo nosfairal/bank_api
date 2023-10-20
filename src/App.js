bca5da3a776ae75c2be9710e585f135e1eae8833
nosfair <nosfairalchtion@gmail.com>
2023-10-20 13:32:11 +0200

    adding redux apiRequest and assets

--- src/App.js
+++ src/App.js
@@ -1,25 +1,47 @@
-import logo from './logo.svg';
-import './App.css';
+import {
+  createBrowserRouter,
+  createRoutesFromElements,
+  Route,
+  RouterProvider,
+} from "react-router-dom";
+import { Provider } from "react-redux";
+import store from "./utils/store";
+import PrivateRoutes from "./components/PrivateRoutes";
+
+import Home from "./pages/Home";
+import Error404 from "./pages/Error404";
+import Layout from "./components/Layout";
+import UserProfil from "./pages/UserProfil";
+import SignIn from "./pages/Login";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+  const router = createBrowserRouter(
+    createRoutesFromElements(
+      <Route path="/" element={<Layout />}>
+        <Route
+          index
+          element={<Home />}
+        />
+
+        <Route path="/login" element={<SignIn />} />
+
+        <Route element={<PrivateRoutes />}>
+          <Route
+            path="/profile"
+            element={<UserProfil />}
+          />
+        </Route>
+
+        <Route path="/*" element={<Error404 />} />
+      </Route>
+    ),
   );
+
+  return (
+    <Provider store={store}>
+      <RouterProvider router={router} />
+    </Provider>
+  )
 }
 
-export default App;
+export default App
